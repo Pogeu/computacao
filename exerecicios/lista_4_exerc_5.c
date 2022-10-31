e89237b8cbc56de301b47d718e2278f30e195579
Pedro Nicollas Bastos <104011631+Pogeu@users.noreply.github.com>
2022-10-31 10:15:40 -0300

    Update lista_4_exerc_5.c

--- exerecicios/lista_4_exerc_5.c
+++ exerecicios/lista_4_exerc_5.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
+#include <locale.h>
 
+float calcuMedia(int qtd)
+{
+   
+    int i, codigo;
+    float soma,valor, media;
+ 
+    for (soma = 0, i= 0; i < qtd; i++)
+    {
+        printf("\nDigitecódigo do produto: ");
+        scanf("%d", &codigo);
+
+        printf("Digite o valor do produto: ");
+        scanf("%f", &valor);
+        
+        soma += valor;
+    }
+    
+    media = soma/qtd;
+    return media;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+
+    int qtd;
+    float media;
+
+    printf("Digite a quantidade de produtos : ");
+    scanf("%d", &qtd);
+    media = calcuMedia(qtd);
+    printf(" \nA média dos produtos é %f .", media);
+
+    return 0;
+}
