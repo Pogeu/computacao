386e02890f941e64929d0c5a52f9a6697412a888
Pedro Nicollas Bastos <104011631+Pogeu@users.noreply.github.com>
2022-10-31 09:46:00 -0300

    Update lista_4_exerc_3.c

--- exerecicios/lista_4_exerc_3.c
+++ exerecicios/lista_4_exerc_3.c
@@ -1,1 +1,41 @@
 
+#include <stdio.h>
+#include <locale.h>
+
+float descobreMaior(int qtd)
+{
+   
+    int i, matricula;
+    float maior,salario;
+ 
+    for (maior= 0, i= 0, i < qtd, i++)
+    {
+        printf("\nDigite sua matricula: ");
+        scanf("%d", &matricula);
+
+        printf("Digite o valor do seu salário: ");
+        scanf("%f", &salario);
+        
+        if (salario > maior)
+        {
+           maior = salario;
+        }
+    }
+   
+    return maior;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+
+    int qtd;
+    float maior;
+
+    printf("Digite a quantidade de funcionarios : ");
+    scanf("%d", &qtd);
+    maior = descobreMaior(qtd);
+    printf(" \nO maior salario é %f .", maior);
+
+    return 0;
+}
