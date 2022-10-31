e43d98d56c26fbf2d5db775a0710a0a1c0446d6b
Pedro Nicollas Bastos <104011631+Pogeu@users.noreply.github.com>
2022-10-31 10:02:02 -0300

    Update lista_4_exerc_4.c

--- exerecicios/lista_4_exerc_4.c
+++ exerecicios/lista_4_exerc_4.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
+#include <locale.h>
 
+float somaSal(int qtd)
+{
+   
+    int i, matricula;
+    float soma,salario;
+ 
+    for (soma= 0; i= 0; i < qtd; i++)
+    {
+        printf("\nDigite sua matricula: ");
+        scanf("%d", &matricula);
+
+        printf("Digite o valor do seu salário: ");
+        scanf("%f", &salario);
+        
+        soma += salario;
+        }
+    }
+   
+    return soma;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+
+    int qtd;
+    float soma;
+
+    printf("Digite a quantidade de funcionarios : ");
+    scanf("%d", &qtd);
+    soma = somaSal(qtd);
+    printf(" \nA soma dos salarios é %f .", soma);
+
+    return 0;
+}
