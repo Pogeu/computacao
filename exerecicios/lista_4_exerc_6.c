05f06f6683462d30b26f95c715dc3ad4ecbf6ee2
Pedro Nicollas Bastos <104011631+Pogeu@users.noreply.github.com>
2022-10-31 10:52:21 -0300

    Update lista_4_exerc_6.c

--- exerecicios/lista_4_exerc_6.c
+++ exerecicios/lista_4_exerc_6.c
@@ -1,1 +1,55 @@
 
+#include <stdio.h>
+#include <locale.h>
+
+float downloadUm(int qtd)
+{
+    int i, j;
+    float tamanho,valor;
+ 
+    for (soma = 0, i= 0, j= 0; i < qtd; i++)
+    {
+        printf("\nDigite tamanho do download: ");
+        scanf("%d", &tamanho);
+        soma += tamanho;
+        
+        if (tamanho >= 2)
+        {
+          j ++;
+        }  
+    }
+    printf("Existem %d downloads de até 2Mbytes", j);
+    return soma;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+
+    int qtddown, qtduser = 0;
+    float soma, preco, valor, media;
+    float somapago = 0;
+  
+    printf("Digite o preço de cada download: ");
+    scanf("%f", &preco);
+    printf("Digite a quantidade de downloads que você deseja : ");
+    scanf("%d", &qtddown);
+    
+    while (qtddown != 0)
+    {
+      soma = dowloadUm(qtddown);
+      valor = soma * preco;
+      printf(" \nO valor pago pelo tamamnho dos downloads é de R$ %f .", valor);
+      
+      somapago += valor
+      qtduser ++;
+      
+      printf("Digite a quantidade de downloads que você deseja : ");
+      scanf("%d", &qtd);
+    }
+    
+    media = somapago/qtduser;
+    printf("O valor médio pago pelos usuarios foi de  R$ %f ." ,media);
+    
+    return 0;
+}
